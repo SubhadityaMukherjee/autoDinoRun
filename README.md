4ec4086593dc99db15384dbe0f8f9393affbec34
Subhaditya <msubhaditya@gmail.com>
2019-11-19 13:14:31 +0530

    changes

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - This automates Chrome Dino Run 
 - Uses OpenCV and Numpy 
+- Run main.py 
+- Note that you might have to modify the screen size 
+
+## To do
+- Use RL
+- Make it more screen size independant
--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import numpy as np
 from PIL import ImageGrab
 import time
 import pyautogui
+from selenium import webdriver
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('start-maximized')
+# driver = webdriver.Chrome(chrome_options = options)
+
+# driver.get("chrome://dino")
 pyautogui.PAUSE = 0
 
 time.sleep(3)
@@ -27,12 +34,17 @@ bbox = (x,y,x+w,y+h) # left top right bottom
 # bbox = (203, 684, 294, 775)
 shape = (bbox[3]-bbox[1], bbox[2]-bbox[0], 3)
 bg = 255
+# ref_img2 = np.full(shape, bg)
 ref_img2 = np.full(shape, bg)
+# ref_img2 = ref_img2[:300,:500]
 i = 1
 
 while True:
     img2 = cv2.cvtColor(np.array(ImageGrab.grab(bbox)), cv2.COLOR_BGR2RGB)
-    # img2 = img2[y:y+h,x:x+w]
+    # driver.save_screenshot('temp.png')
+    # img2 = cv2.cvtColor(cv2.imread('temp.png'), cv2.COLOR_BGR2RGB)
+    img2 = img2[:300,:500]
+    # cann = cv2.Canny(img2,threshold1=200,threshold2=300)
 
     if bg != img2[0][0][0]:
         bg = img2[0][0][0]
@@ -48,17 +60,10 @@ while True:
     if i % 4 == 0:
         bbox = (bbox[0]+1, bbox[1], bbox[2]+1, bbox[3])
         shape = (bbox[3]-bbox[1], bbox[2]-bbox[0], 3)
-        ref_img2 = np.full(shape, bg)
+        ref_img2 = np.full(shape, bg)[:300,:500]
         print(f"update {i}")
         i += 1
 
-    # if cv2.waitKey(1) == 27:
-    #     break
-
     counter+=1
 
-# cv2.destroyAllWindows()
-
-# img = cv2.imread('images/full_11.jpg')
-# r = cv2.selectROI(img)
-# print(r)
+# driver.close()
